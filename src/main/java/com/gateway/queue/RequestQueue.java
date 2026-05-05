75408f43fddd390135d157f61cb36e32604ac6e7
Pranali-Jadhav <pranalidjadhav2002@gmail.com>
2026-05-05 21:36:04 +0530

    recommiting queue class

--- src/main/java/com/gateway/queue/RequestQueue.java
+++ src/main/java/com/gateway/queue/RequestQueue.java
@@ -1,4 +1,28 @@
 package com.gateway.queue;
+import com.gateway.model.Request;
+
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class RequestQueue {
+
+    private final PriorityBlockingQueue<Request> queue;
+
+    public RequestQueue()
+    {
+        this.queue = new PriorityBlockingQueue<>();
+    }
+  //“Blocking methods like take() and put() can be interrupted while waiting, so they throw InterruptedException
+  // to signal that the thread was interrupted during execution.”
+    public void addRequest(Request r) throws InterruptedException
+    {
+        queue.put(r);
+    }
+    public Request getRequest() throws InterruptedException
+    {
+        return queue.take();
+    }
+    public int size() {
+        return queue.size();
+    }
+
 }
